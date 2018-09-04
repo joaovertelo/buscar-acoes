9d487045909c77fdee8717852232683d3271b550
joao.pimenta <joao.pimenta@groupsoftware.com.br>
2018-09-04 16:41:58 -0300

    configurando rotas para github pages

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import Home from './modules/home/Home';
-const Contato = () => <div> contato</div>;
 
 class Routes extends Component {
 	render() {
 		return (
 			<React.Fragment>
-				<Route path="/" exact component={Home} />
-				<Route path="/contato" exact component={Contato} />
+				<Route path={'/'} exact component={Home} />
 			</React.Fragment>
 		);
 	}
