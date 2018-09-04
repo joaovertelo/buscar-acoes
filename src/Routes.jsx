afb8babfe1ab6484fa6407ca3588a945b6c2d2de
joao.pimenta <joao.pimenta@groupsoftware.com.br>
2018-09-04 14:55:12 -0300

    Teste rotas

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 
 import Home from './modules/home/Home';
+const Contato = () => <div> contato</div>;
 
 class Routes extends Component {
 	render() {
 		return (
 			<React.Fragment>
 				<Route path="/" exact component={Home} />
+				<Route path="/contato" exact component={Contato} />
 			</React.Fragment>
 		);
 	}
